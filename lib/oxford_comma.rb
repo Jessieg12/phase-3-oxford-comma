228b8df7b61f2cbcfe20aa3da686f003bfe0bd96
Jessie Guzman <jessieguzman777@gmail.com>
2023-11-01 22:09:02 -0500

    commit tree e59ac972477d9632924c0a8a8c01082d381fd283

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comm(array)
+   if array.length == 1
+    return array[0]
+   elsif array.length == 2
+    combine = array.join(" and ")
+    return combine
+   else
+    range = array[0..-2].join(", ")
+    combine = "#{range}, and #{array[-1]}"
+    return combine
+  end
+end
+
+def oxford_comma(array)
+  return array.join(" and ") if array.size < 3
+
+  # insert 'and ' in front of the last string in the array
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ")  
 end
